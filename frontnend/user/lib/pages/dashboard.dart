b085ad38ab2cd1d10dc3d8f608eb41b10278da3b
riyakhadgi <np03cs4s220082@heraldcollege.edu.np>
2024-03-19 14:16:14 +0545

    fixed dashboard ui

--- frontnend/user/lib/pages/dashboard.dart
+++ frontnend/user/lib/pages/dashboard.dart
@@ -1,61 +1,101 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class Dashboard extends StatelessWidget {
-  const Dashboard({Key? key}) : super(key: key);
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    MapController controller = MapController(
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+    Widget build(BuildContext context) {
+    return Scaffold (
+      appBar:AppBar(
+        leading: SvgPicture.asset("logo/logo.svg",width:200,height:200),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.menu),)
+        ],
+      ),
+      body:body()
+    );
+  }
+   MapController controller = MapController(
       initPosition: GeoPoint(latitude: 27.7122811, longitude: 85.3309045), // Kathmandu, Nepal
     );
-
-    return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.menu),
-      ),
-      body: OSMFlutter(
-        controller: controller,
-        osmOption: OSMOption(
-          userTrackingOption: const UserTrackingOption(
-            enableTracking: true,
-            unFollowUser: false,
-          ),
-          zoomOption: const ZoomOption(
-            initZoom: 8,
-            minZoomLevel: 3,
-            maxZoomLevel: 19,
-            stepZoom: 1.0,
-          ),
-          userLocationMarker: UserLocationMaker(
-            personMarker: const MarkerIcon(
-              icon: Icon(
-                Icons.location_history_rounded,
-                color: Colors.red,
-                size: 48,
-              ),
-            ),
-            directionArrowMarker: const MarkerIcon(
-              icon: Icon(
-                Icons.double_arrow,
-                size: 48,
-              ),
+  SingleChildScrollView body(){
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+        
+            height: 300,
+            child:  OSMFlutter(
+      controller: controller,
+      osmOption: OSMOption(
+        userTrackingOption: const UserTrackingOption(
+          enableTracking: true,
+          unFollowUser: false,
+        ),
+        zoomOption: const ZoomOption(
+          initZoom: 8,
+          minZoomLevel: 3,
+          maxZoomLevel: 19,
+          stepZoom: 1.0,
+        ),
+        userLocationMarker: UserLocationMaker(
+          personMarker: const MarkerIcon(
+            icon: Icon(
+              Icons.location_history_rounded,
+              color: Colors.red,
+              size: 48,
             ),
           ),
-          roadConfiguration: const RoadOption(
-            roadColor: Colors.yellowAccent,
+          directionArrowMarker: const MarkerIcon(
+            icon: Icon(
+              Icons.double_arrow,
+              size: 48,
+            ),
           ),
-          markerOption: MarkerOption(
-            defaultMarker: const MarkerIcon(
-              icon: Icon(
-                Icons.person_pin_circle,
-                color: Colors.blue,
-                size: 56,
-              ),
+        ),
+        roadConfiguration: const RoadOption(
+          roadColor: Colors.yellowAccent,
+        ),
+        markerOption: MarkerOption(
+          defaultMarker: const MarkerIcon(
+            icon: Icon(
+              Icons.person_pin_circle,
+              color: Colors.blue,
+              size: 56,
             ),
           ),
         ),
       ),
-    );
+            ),
+          ),
+          Container(
+            child: Column(children: [
+             TextField( decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Pickup Location',
+        ),
+             ),
+        TextField( decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Destination',
+        ),),
+        TextField( decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Enter your fare here',
+        ),),
+              ElevatedButton(onPressed: (){}, child: Text("Find Driver"))
+        ]),
+        )
+        ]
+
+        )
+        ); 
   }
 }
--- frontnend/user/lib/pages/pickuppage.dart
+++ frontnend/user/lib/pages/pickuppage.dart
@@ -1,77 +1,101 @@
-import 'package:flutter/material.dart'; 
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
-class pickuppage extends StatefulWidget {
-  const pickuppage({super.key});
+// import 'package:flutter/material.dart';
+// import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 
-  @override
-  State<pickuppage> createState() => _pickuppageState();
-}
+// class Dashboard extends StatefulWidget {
+//   const Dashboard({super.key});
 
-class _pickuppageState extends State<pickuppage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold (
-      appBar:AppBar(
-        title: Text("Book Gar"),
-        centerTitle: true,
-      ),
-      body:body()
-    );
-  }
-   MapController controller = MapController(
-      initPosition: GeoPoint(latitude: 27.7122811, longitude: 85.3309045), // Kathmandu, Nepal
-    );
-  SingleChildScrollView body(){
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-            width: 500,
-            height: 500,
-            child:  OSMFlutter(
-      controller: controller,
-      osmOption: OSMOption(
-        userTrackingOption: const UserTrackingOption(
-          enableTracking: true,
-          unFollowUser: false,
-        ),
-        zoomOption: const ZoomOption(
-          initZoom: 8,
-          minZoomLevel: 3,
-          maxZoomLevel: 19,
-          stepZoom: 1.0,
-        ),
-        userLocationMarker: UserLocationMaker(
-          personMarker: const MarkerIcon(
-            icon: Icon(
-              Icons.location_history_rounded,
-              color: Colors.red,
-              size: 48,
-            ),
-          ),
-          directionArrowMarker: const MarkerIcon(
-            icon: Icon(
-              Icons.double_arrow,
-              size: 48,
-            ),
-          ),
-        ),
-        roadConfiguration: const RoadOption(
-          roadColor: Colors.yellowAccent,
-        ),
-        markerOption: MarkerOption(
-          defaultMarker: const MarkerIcon(
-            icon: Icon(
-              Icons.person_pin_circle,
-              color: Colors.blue,
-              size: 56,
-            ),
-          ),
-        ),
-      ),
-            ),
-          )
-        ],),
-    );
-  }
-}
+//   @override
+//   State<Dashboard> createState() => _DashboardState();
+// }
+
+// class _DashboardState extends State<Dashboard> {
+//   @override
+//     Widget build(BuildContext context) {
+//     return Scaffold (
+//       appBar:AppBar(
+//         leading: SvgPicture.asset("logo/logo.svg",width:200,height:200),
+//         actions: [
+//           IconButton(onPressed: (){}, icon: Icon(Icons.menu),)
+//         ],
+//       ),
+//       body:body()
+//     );
+//   }
+//    MapController controller = MapController(
+//       initPosition: GeoPoint(latitude: 27.7122811, longitude: 85.3309045), // Kathmandu, Nepal
+//     );
+//   SingleChildScrollView body(){
+//     return SingleChildScrollView(
+//       child: Column(
+//         children: [
+//           Container(
+        
+//             height: 300,
+//             child:  OSMFlutter(
+//       controller: controller,
+//       osmOption: OSMOption(
+//         userTrackingOption: const UserTrackingOption(
+//           enableTracking: true,
+//           unFollowUser: false,
+//         ),
+//         zoomOption: const ZoomOption(
+//           initZoom: 8,
+//           minZoomLevel: 3,
+//           maxZoomLevel: 19,
+//           stepZoom: 1.0,
+//         ),
+//         userLocationMarker: UserLocationMaker(
+//           personMarker: const MarkerIcon(
+//             icon: Icon(
+//               Icons.location_history_rounded,
+//               color: Colors.red,
+//               size: 48,
+//             ),
+//           ),
+//           directionArrowMarker: const MarkerIcon(
+//             icon: Icon(
+//               Icons.double_arrow,
+//               size: 48,
+//             ),
+//           ),
+//         ),
+//         roadConfiguration: const RoadOption(
+//           roadColor: Colors.yellowAccent,
+//         ),
+//         markerOption: MarkerOption(
+//           defaultMarker: const MarkerIcon(
+//             icon: Icon(
+//               Icons.person_pin_circle,
+//               color: Colors.blue,
+//               size: 56,
+//             ),
+//           ),
+//         ),
+//       ),
+//             ),
+//           ),
+//           Container(
+//             child: Column(children: [
+//              TextField( decoration: InputDecoration(
+//           border: OutlineInputBorder(),
+//           labelText: 'Pickup Location',
+//         ),
+//              ),
+//         TextField( decoration: InputDecoration(
+//           border: OutlineInputBorder(),
+//           labelText: 'Destination',
+//         ),),
+//         TextField( decoration: InputDecoration(
+//           border: OutlineInputBorder(),
+//           labelText: 'Enter your fare here',
+//         ),),
+//           ElevatedButton(onPressed: (){}, child: Text("Find Driver"))
+//         ]),
+//         )
+//         ]
+
+//         )
+//         ); 
+//   }
+// }
